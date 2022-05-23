45ba8dc2ab195f6b2ebb84efd0c394c6c09394be
Mac Signer <macsigner@gmx.ch>
2022-05-23 10:29:53 +0200

    Add delegate function

--- tools.js
+++ tools.js
@@ -1,3 +1,11 @@
 export function removeElement(el) {
     el.remove();
 }
+
+export function delegate(selector, fn) {
+    return function(e) {
+        if(e.target.matches(selector)) {
+            fn.call(this, e);
+        }
+    }
+}
